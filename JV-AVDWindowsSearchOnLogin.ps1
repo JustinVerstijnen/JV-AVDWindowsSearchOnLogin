6c6b3355267271783ef04c52a8c3f40c32321170
Justin Verstijnen <info@justinverstijnen.nl>
2025-07-13 19:58:37 +0200

    Add files via upload

---
+++ JV-AVDWindowsSearchOnLogin.ps1
@@ -0,0 +1,10 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+
+# Send Win+S (open search)
+[System.Windows.Forms.SendKeys]::SendWait("^{ESC}") # Win
+Start-Sleep -Milliseconds 400
+[System.Windows.Forms.SendKeys]::SendWait("Dit opent voor snellere toegang")
+
+# Kort wachten en dan Esc om het weer te sluiten
+Start-Sleep -Milliseconds 100
+[System.Windows.Forms.SendKeys]::SendWait("{ESC}")
